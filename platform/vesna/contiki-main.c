bc9f70d7435ad2797357a91a0bcbf95ea65e89ad
Tomaz Solc <tomaz.solc@ijs.si>
2011-12-09 14:49:52 +0100

    Working USART1.

--- platform/vesna/contiki-main.c
+++ platform/vesna/contiki-main.c
@@ -1,7 +1,3 @@
-#include <stm32f10x_map.h>
-#include <stm32f10x_dma.h>
-#include <gpio.h>
-#include <nvic.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <debug-uart.h>
@@ -11,11 +7,81 @@
 #include <sys/autostart.h>
 #include <clock.h>
 
+#include <libopencm3/stm32/f1/rcc.h>
+#include <libopencm3/stm32/f1/gpio.h>
+#include <libopencm3/stm32/usart.h>
+#include <libopencm3/stm32/nvic.h>
+#include <stdio.h>
+#include <errno.h>
+
+void clock_setup(void)
+{
+  rcc_clock_setup_in_hsi_out_48mhz();
+
+	/* Enable clocks for GPIO port A (for GPIO_USART1_TX) and USART1. */
+	rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPAEN |
+				    RCC_APB2ENR_AFIOEN | RCC_APB2ENR_USART1EN);
+}
+
+void usart_setup(void)
+{
+	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ,
+		      GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO9);
+
+	/* Setup UART parameters. */
+	usart_set_baudrate(USART1, 19200);
+	usart_set_databits(USART1, 8);
+	usart_set_stopbits(USART1, USART_STOPBITS_1);
+	usart_set_parity(USART1, USART_PARITY_NONE);
+	usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
+	usart_set_mode(USART1, USART_MODE_TX);
+
+	/* Finally enable the USART. */
+	usart_enable(USART1);
+}
+
+int _write(int file, char *ptr, int len)
+{
+	int i;
+
+	if (file == 1) {
+		for (i = 0; i < len; i++)
+			usart_send_blocking(USART1, ptr[i]);
+		return i;
+	}
+
+	errno = EIO;
+	return -1;
+}
+
+/*
+int main(void)
+{
+	int counter = 0;
+	float fcounter = 0.0;
+	double dcounter = 0.0;
+
+	while (1) {
+		gpio_toggle(GPIOC, GPIO12);
+		printf("Hello World! %i %f %f\r\n", counter, fcounter,
+		       dcounter);
+		counter++;
+		fcounter += 0.01;
+		dcounter += 0.01;
+	}
+
+	return 0;
+}
+*/
+
 unsigned int idle_count = 0;
 
 int
 main()
 {
+	clock_setup();
+	usart_setup();
+
   //dbg_setup_uart();
   printf("Initialising\n");
   
@@ -34,7 +100,4 @@ main()
   }
   return 0;
 }
-
-
-
 
