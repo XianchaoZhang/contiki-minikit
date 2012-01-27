4a150fa8867e09a3713da1a2a851e09d67da656a
Tomaz Solc <tomaz.solc@tablix.org>
2012-01-27 15:46:41 +0100

    Cleaned up contiki-conf.h

--- platform/vesna/contiki-conf.h
+++ platform/vesna/contiki-conf.h
@@ -3,9 +3,6 @@
 
 #include <stdint.h>
 
-#define CCIF
-#define CLIF
-
 #define WITH_UIP 1
 #define WITH_ASCII 1
 
@@ -20,18 +17,6 @@ typedef int32_t s32_t;
 typedef unsigned int clock_time_t;
 typedef unsigned int uip_stats_t;
 
-#ifndef BV
-#define BV(x) (1<<(x))
-#endif
-
-/* uIP configuration */
-#define UIP_CONF_LLH_LEN         0
-#define UIP_CONF_BROADCAST       1
-#define UIP_CONF_LOGGING 1
-#define UIP_CONF_BUFFER_SIZE 116
-
-#define UIP_CONF_TCP_FORWARD 1
-
 /* this is used to define the local link-local IPv6 address */
 #define SLIP_LINKLEVEL_ADDR 0x00,0x00,0x00,0x00,0x00,0x02
 
@@ -39,13 +24,4 @@ typedef unsigned int uip_stats_t;
  * to know the IP of the remote computer. */
 #define SLIP_REMOTE_IP uip_ip6addr(&ipaddr,0xfe80,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0001)
 
-/* Prefix for relocation sections in ELF files */
-#define REL_SECT_PREFIX ".rel"
-
-#define CC_BYTE_ALIGNED __attribute__ ((packed, aligned(1)))
-
-#define USB_EP1_SIZE 64
-#define USB_EP2_SIZE 64
-
-#define RAND_MAX 0x7fff
 #endif /* __CONTIKI_CONF_H__CDBB4VIH3I__ */
